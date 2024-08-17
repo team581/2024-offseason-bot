bb8d8774bac223b79d1383786812d85e0fe6ebb9
Owen <117956892+Owen756@users.noreply.github.com>
2024-08-17 12:53:13 -0700

    Add vision subsystem

---
+++ src/main/java/frc/robot/vision/VisionSubsystem.java
@@ -0,0 +1,54 @@
+package frc.robot.vision;
+
+import frc.robot.imu.ImuSubsystem;
+import frc.robot.util.scheduling.SubsystemPriority;
+import frc.robot.util.state_machines.StateMachine;
+import frc.robot.vision.LimelightHelpers.PoseEstimate;
+import java.util.Optional;
+
+public class VisionSubsystem extends StateMachine<VisionState> {
+  private final ImuSubsystem imu;
+  private Optional<VisionResult> rawVisionResult;
+  private Optional<VisionResult> processedVisionResult;
+  private PoseEstimate estimatePose;
+
+  public VisionSubsystem(ImuSubsystem imu) {
+    super(SubsystemPriority.VISION, VisionState.DEFAULT_STATE);
+    this.imu = imu;
+  }
+
+  private Optional<VisionResult> getRawVisionResult() {
+    var estimatePose = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("");
+
+    if (estimatePose == null) {
+      return Optional.empty();
+    }
+
+    if (estimatePose.tagCount == 0) {
+      return Optional.empty();
+    }
+
+    // This prevents pose estimator from having crazy poses if the Limelight loses power
+    if (estimatePose.pose.getX() == 0.0 && estimatePose.pose.getY() == 0.0) {
+      return Optional.empty();
+    }
+
+    return Optional.of(new VisionResult(estimatePose.pose, estimatePose.timestampSeconds));
+  }
+
+  @Override
+  protected void collectInputs() {
+    rawVisionResult = getRawVisionResult();
+    if (rawVisionResult.isEmpty()) {
+      processedVisionResult = Optional.empty();
+    } else {
+      var rawData = rawVisionResult.get();
+      new VisionResult(VisionUtil.interpolatePose(rawData.pose()), rawData.timestamp());
+    }
+  }
+
+  @Override
+  public void robotPeriodic() {
+    super.robotPeriodic();
+  }
+}
---
+++ src/main/java/frc/robot/vision/VisionUtil.java
@@ -0,0 +1,66 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.vision;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import java.util.List;
+
+public class VisionUtil {
+  private static final VisionInterpolationData SUBWOOFER =
+      new VisionInterpolationData(
+          new Pose2d(15.19, 5.54, new Rotation2d(0)),
+          new Pose2d(15.23, 5.45, new Rotation2d(0)),
+          "Subwoofer");
+  private static final VisionInterpolationData STAGE_FRONT =
+      new VisionInterpolationData(
+          new Pose2d(12.85, 4.71, new Rotation2d(0)),
+          new Pose2d(12.83, 4.66, new Rotation2d(0)),
+          "StageFront");
+  private static final VisionInterpolationData AMP_SIDE_STAGE =
+      new VisionInterpolationData(
+          new Pose2d(10.27, 7.79, new Rotation2d(0)),
+          new Pose2d(10.36, 7.74, new Rotation2d(0)),
+          "AMP_SIDE_STAGE");
+  private static final VisionInterpolationData STAGE_MIDDLE =
+      new VisionInterpolationData(
+          new Pose2d(11.98, 5.16, new Rotation2d(0)),
+          new Pose2d(12.0, 5.11, new Rotation2d(0)),
+          "StageMiddle");
+
+  private static final List<VisionInterpolationData> DATA_POINTS =
+      List.of(SUBWOOFER, STAGE_FRONT, AMP_SIDE_STAGE, STAGE_MIDDLE);
+
+  /**
+   * @param visionInput - pose from the limelight
+   * @return a transformed pose that can be added to the pose estimator
+   */
+  public static Pose2d interpolatePose(Pose2d visionInput) {
+    double distanceSum = 0;
+    for (var dataPoint : DATA_POINTS) {
+      double distancePoint =
+          dataPoint.visionPose().getTranslation().getDistance(visionInput.getTranslation());
+
+      distanceSum += distancePoint;
+    }
+    double weightedX = 0;
+    double weightedY = 0;
+
+    for (var dataPoint : DATA_POINTS) {
+      double distancePoint =
+          dataPoint.visionPose().getTranslation().getDistance(visionInput.getTranslation());
+      var weight = 1 - ((distanceSum - distancePoint) / distanceSum);
+      var result = dataPoint.visionPose().minus(dataPoint.measuredPose()).times(weight);
+      weightedX += result.getX();
+      weightedY += result.getY();
+    }
+    Pose2d interpolatedSum =
+        new Pose2d(
+            visionInput.getX() + weightedX,
+            visionInput.getY() + weightedY,
+            visionInput.getRotation());
+    return interpolatedSum;
+  }
+}
