f6ba68709d25504905b325a916b6a358ae9f1569
Jonah Snider <jonah@jonahsnider.com>
2024-09-08 13:22:14 -0700

    Update subsystem priorities for new StateMachine pattern

--- src/main/java/frc/robot/util/scheduling/SubsystemPriority.java
+++ src/main/java/frc/robot/util/scheduling/SubsystemPriority.java
@@ -1,26 +1,28 @@
 package frc.robot.util.scheduling;
 
 public enum SubsystemPriority {
-  // Vision must run before **everything** to ensure that the cached data is fresh
-  VISION(50),
+  // 20-29 is for sensor subsystems
 
-  NOTE_TRACKING(41),
-  AUTOS(40),
+  // IMU runs before vision so that it has fresh data to pass to MegaTag2
+  IMU(22),
+  // Vision runs before localization so that it has fresh vision data for pose estimator
+  VISION(21),
+  // Localization runs before arm and shooter so that they have fresh speaker distance values
+  LOCALIZATION(20),
+  FMS(20),
 
-  ROBOT_MANAGER(30),
+  // 10-19 is for actuator subsystems
 
-  SNAPS(20),
-
-  SWERVE(10),
-  IMU(10),
-  SHOOTER(10),
-  LOCALIZATION(10),
-  INTAKE(10),
-  QUEUER(10),
   ARM(10),
+  SHOOTER(10),
+  QUEUER(10),
+  INTAKE(10),
 
-  FMS(0),
-  RUMBLE_CONTROLLER(0);
+  // 0-9 is for manager subsystems
+
+  // Robot manager runs last so that all sensor data is fresh before processing state transitions
+  ROBOT_MANAGER(0),
+  AUTOS(0);
 
   final int value;
 
