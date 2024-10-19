95d9a96e0a6d7994e4387b1cd4454dde6481b695
Owen <117956892+Owen756@users.noreply.github.com>
2024-10-19 11:39:08 -0700

    Interpolated vision (#69)
    * work on implement two limelight interpolate vision
    
    * double limelight interpolated vision
    
    * format with spotless
    
    * Remove accidental committed file
    
    * implement interpolated vision
    
    ---------
    
    Co-authored-by: owenrobotics581 <owenrobotics581@gmail.com>
    Co-authored-by: Jordan <117786700+jordanjcoderman@users.noreply.github.com>

--- src/main/java/frc/robot/vision/Limelight.java
+++ src/main/java/frc/robot/vision/Limelight.java
@@ -1,21 +1,23 @@
 package frc.robot.vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import frc.robot.vision.LimelightHelpers.LimelightResults;
+import frc.robot.vision.interpolation.CameraDataset;
 import frc.robot.vision.interpolation.InterpolatedVision;
+
 import java.util.Optional;
 
 import dev.doglog.DogLog;
 
 public class Limelight {
-  private Optional<VisionResult> rawVisionResult;
-  private Optional<VisionResult> processedVisionResult;
   private final String limelightTableName;
   private final String name;
+  private CameraDataset interpolationData;
+;
 
-  public Limelight(String name) {
+  public Limelight(String name, CameraDataset interpolationData) {
     limelightTableName = "limelight-" + name;
     this.name = name;
+    this.interpolationData = interpolationData;
   }
 
   public Optional<VisionResult> getInterpolatedVisionResult() {
@@ -25,7 +27,7 @@ public class Limelight {
       return Optional.empty();
     }
     
-    Pose2d interpolatedPose = InterpolatedVision.interpolatePose(rawResult.get().pose());
+    Pose2d interpolatedPose = InterpolatedVision.interpolatePose(rawResult.get().pose(), interpolationData);
     return Optional.of(new VisionResult(interpolatedPose, rawResult.get().timestamp()));
   }
 
