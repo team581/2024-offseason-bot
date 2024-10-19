6204929831b6adfe6bd5f2cd0bf342df4cb8efed
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2024-10-19 11:38:09 -0700

    implement interpolated vision

--- src/main/java/frc/robot/vision/interpolation/InterpolatedVision.java
+++ src/main/java/frc/robot/vision/interpolation/InterpolatedVision.java
@@ -5,22 +5,7 @@ import frc.robot.config.RobotConfig;
 import frc.robot.fms.FmsSubsystem;
 
 public class InterpolatedVision {
-  private static final InterpolatedVisionDataset dataSet =
-      RobotConfig.get().vision().interpolatedVisionSet();
-
-  /**
-   * @param visionInput - pose from the limelight
-   * @return a transformed pose that can be added to the pose estimator
-   */
-  public static Pose2d leftInterpolatePose(Pose2d visionInput) {
-    return interpolatePose(visionInput, dataSet.leftSet);
-  }
-
-  public static Pose2d rightInterpolatePose(Pose2d visionInput) {
-    return interpolatePose(visionInput, dataSet.rightSet);
-  }
-
-  private static Pose2d interpolatePose(Pose2d visionInput, CameraDataset dataset) {
+  public static Pose2d interpolatePose(Pose2d visionInput, CameraDataset dataset) {
     var usedDataPoints = FmsSubsystem.isRedAlliance() ? dataset.red() : dataset.blue();
 
     return new Pose2d(
