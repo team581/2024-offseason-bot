6b97004089e83c4aab8e04b2756957a6fd903e9c
ryanknj5 <140574269+ryanknj5@users.noreply.github.com>
2024-10-28 18:49:24 -0700

    continue lights subsystem

--- src/main/java/frc/robot/lights/LightsSubsystem.java
+++ src/main/java/frc/robot/lights/LightsSubsystem.java
@@ -1,15 +1,32 @@
 package frc.robot.lights;
 
+import com.ctre.phoenix.led.CANdle;
+import dev.doglog.DogLog;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import frc.robot.robot_manager.RobotManager;
 import frc.robot.util.scheduling.LifecycleSubsystem;
 import frc.robot.util.scheduling.SubsystemPriority;
+import frc.robot.vision.LimelightHelpers;
 
 public class LightsSubsystem extends LifecycleSubsystem {
   private BlinkPattern blinkPattern;
   private Color color;
+  private final CANdle candle;
 
-  public LightsSubsystem() {
+  private final Timer blinkTimer = new Timer();
+  private final Timer lightsOnExitTimer = new Timer();
+
+  private final RobotManager robotManager;
+  private static final double FAST_BLINK_DURATION = 0.08;
+  private static final double SLOW_BLINK_DURATION = 0.25;
+
+  public LightsSubsystem(RobotManager robotManager, CANdle candle) {
     super(SubsystemPriority.LIGHTS);
+    this.robotManager = robotManager;
+    this.candle = candle;
+    blinkTimer.start();
   }
 
   @Override
@@ -18,7 +35,7 @@ public class LightsSubsystem extends LifecycleSubsystem {
 
     // Continuous state actions
     switch (robotManager.getState()) {
-      case INTAKING -> {
+      case INTAKING, INTAKE_ASSIST -> {
         blinkPattern = BlinkPattern.SOLID;
         color = Color.kRed;
       }
@@ -26,8 +43,84 @@ public class LightsSubsystem extends LifecycleSubsystem {
         blinkPattern = BlinkPattern.SOLID;
         color = Color.kYellow;
       }
+      case IDLE_WITH_GP -> {
+        blinkPattern = BlinkPattern.SOLID;
+        color = Color.kWhite;
+      }
+
+      case AMP_WAITING, PODIUM_WAITING, FEEDING_WAITING, SUBWOOFER_WAITING, SPEAKER_WAITING -> {
+        blinkPattern = BlinkPattern.SOLID;
+        color = Color.kRed;
+      }
+      case AMP_PREPARE_TO_SCORE,
+          PASS_PREPARE_TO_SHOOT,
+          PODIUM_PREPARE_TO_SCORE,
+          FEEDING_PREPARE_TO_SHOOT,
+          SPEAKER_PREPARE_TO_SCORE,
+          SUBWOOFER_PREPARE_TO_SCORE,
+          OUTTAKING -> {
+        blinkPattern = BlinkPattern.SOLID;
+        color = Color.kYellow;
+      }
+      case AMP_SCORING,
+          PODIUM_SCORING,
+          PASS_SHOOTING,
+          SPEAKER_SCORING,
+          SUBWOOFER_SCORING,
+          FEEDING_SHOOTING -> {
+        blinkPattern = BlinkPattern.SOLID;
+        color = Color.kGreen;
+      }
+
+      case CLIMBING_1_LINEUP -> {
+        blinkPattern = BlinkPattern.SOLID;
+        color = Color.kRed;
+      }
+      case CLIMBING_2_HANGING -> {
+        blinkPattern = BlinkPattern.SOLID;
+        color = Color.kGreen;
+      }
+      case UNJAM -> {
+        blinkPattern = BlinkPattern.BLINK_FAST;
+        color = Color.kRed;
+      }
+      case IDLE_NO_GP -> {
+        blinkPattern = BlinkPattern.SOLID;
+        color = Color.kBlack;
+      }
+    }
+    DogLog.log("Lights/Color", color.toString());
+    DogLog.log("Lights/Pattern", blinkPattern);
+
+    Color8Bit color8Bit = new Color8Bit(color);
+
+    if (color == Color.kWhite) {
+      LimelightHelpers.setLEDMode_ForceBlink("limelight-note");
+    } else {
+      LimelightHelpers.setLEDMode_ForceOff("limelight-note");
     }
 
-    // TODO: Do something with the blink & color (set CANdle)
+    if (blinkPattern == BlinkPattern.SOLID) {
+      candle.setLEDs(color8Bit.red, color8Bit.green, color8Bit.blue);
+    } else {
+      double time = blinkTimer.get();
+      double onDuration = 0;
+      double offDuration = 0;
+
+      if (blinkPattern == BlinkPattern.BLINK_FAST) {
+        onDuration = FAST_BLINK_DURATION;
+        offDuration = FAST_BLINK_DURATION * 2;
+      } else if (blinkPattern == BlinkPattern.BLINK_SLOW) {
+        onDuration = SLOW_BLINK_DURATION;
+        offDuration = SLOW_BLINK_DURATION * 2;
+      }
+
+      if (time >= offDuration) {
+        blinkTimer.reset();
+        candle.setLEDs(0, 0, 0);
+      } else if (time >= onDuration) {
+        candle.setLEDs(color8Bit.red, color8Bit.green, color8Bit.blue);
+      }
+    }
   }
 }
