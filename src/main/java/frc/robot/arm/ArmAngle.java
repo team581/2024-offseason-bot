fba61effe178c5afc65e3f653d3c8680243cd722
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-10-29 21:00:28 -0700

    change climb down arm angle

--- src/main/java/frc/robot/arm/ArmAngle.java
+++ src/main/java/frc/robot/arm/ArmAngle.java
@@ -6,7 +6,7 @@ public class ArmAngle {
   public static final double PODIUM = -40.0;
   public static final double IDLE = -77.0;
   public static final double CLIMBING_1_LINEUP = 90.0;
-  public static final double CLIMBING_2_HANGING = -25.0;
+  public static final double CLIMBING_2_HANGING = -31.0;
   public static final double AMP = 65.0;
   public static final double PASS = -60.0;
 }
