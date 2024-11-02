08932a1867fe9cdd2b854d23924783a07761321e
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-11-02 10:22:36 -0700

    start dynamic auto commands

--- src/main/java/frc/robot/autos/AutoCommands.java
+++ src/main/java/frc/robot/autos/AutoCommands.java
@@ -1,15 +1,60 @@
 package frc.robot.autos;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.fms.FmsSubsystem;
 import frc.robot.robot_manager.RobotCommands;
 import frc.robot.robot_manager.RobotManager;
+import frc.robot.robot_manager.RobotState;
 
 public class AutoCommands {
+  private static Command followPathForAlliance(PathPlannerPath redPath, PathPlannerPath bluePath) {
+    return Commands.either(
+        AutoBuilder.followPath(redPath),
+        AutoBuilder.followPath(bluePath),
+        FmsSubsystem::isRedAlliance);
+  }
 
-  public AutoCommands(RobotCommands robotCommands, RobotManager robotManager) {}
+  private final RobotCommands robotCommands;
+  private final RobotManager robotManager;
+  private static final boolean USE_DYNAMIC_AUTOS = true;
+
+  public AutoCommands(RobotCommands robotCommands, RobotManager robotManager) {
+    this.robotCommands = robotCommands;
+    this.robotManager = robotManager;
+  }
 
   public Command doNothingCommand() {
     return Commands.none();
+  }
+
+  private boolean hasNote() {
+    if (!USE_DYNAMIC_AUTOS) {
+      return true;
+    }
+    return robotManager.queuer.hasNote()
+        || robotManager.getState().
+        || robotManager.getState() == RobotState.IDLE_WITH_GP; }
+
+  public Command dynamicRedAmp5PieceCommand() {
+    var red4To5 = PathPlannerPath.fromChoreoTrajectory("Red 4 to 5");
+    var red5ToAmp = PathPlannerPath.fromChoreoTrajectory("Red 5 to Amp");
+    var red4ToAmp = PathPlannerPath.fromChoreoTrajectory("Red Amp 5 Piece.");
+
+    return Commands.sequence(
+        Commands.either(
+            followPathForAlliance(red4ToAmp, red4ToAmp)
+                .andThen(robotCommands.speakerCommand())
+                .andThen(followPathForAlliance(red5ToAmp, red5ToAmp)),
+            followPathForAlliance(red4To5, red4To5),
+            null),
+        Commands.either(
+            followPathForAlliance(red4ToAmp, red4ToAmp)
+                .andThen(robotCommands.speakerCommand())
+                .andThen(followPathForAlliance(red5ToAmp, red5ToAmp)),
+            followPathForAlliance(red4To5, red4To5),
+            null));
   }
 }
