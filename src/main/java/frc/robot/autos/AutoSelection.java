63b37180aff36b5d252103a265c64160bfb66d2d
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-11-08 14:55:25 -0800

    Add blue autos to auto selection

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -2,9 +2,9 @@ package frc.robot.autos;
 
 public enum AutoSelection {
   DO_NOTHING("", ""),
-  AMP_5_PIECE("Red Amp 5 Piece", ""),
-  SOURCE_4_PIECE("Red Source 4 Piece", ""),
-  OP("Red OP", "");
+  AMP_5_PIECE("Red Amp 5 Piece", "Blue Amp 5 Piece"),
+  SOURCE_4_PIECE("Red Source 4 Piece", "Blue Source 4 Piece"),
+  OP("Red OP", "Blue OP");
 
   public final String redAutoName;
   public final String blueAutoName;
