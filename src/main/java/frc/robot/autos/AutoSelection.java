93d0d597a1addc5088420adbc7803f416a1a8345
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-11-10 11:31:41 -0800

    Blue & Red source 4 piece offset auto

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -4,6 +4,7 @@ public enum AutoSelection {
   DO_NOTHING("", ""),
   AMP_5_PIECE("Red Amp 5 Piece", "Blue Amp 5 Piece"),
   SOURCE_4_PIECE("Red Source 4 Piece", "Blue Source 4 Piece"),
+  SOURCE_OFFSET("Red Source Offset", "Blue Source Offset"),
   OP("Red OP", "Blue OP");
 
   public final String redAutoName;
