d7501754199fe016b16120ab8c9427b998a910b0
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-11-10 11:48:44 -0800

    Red & Blue 5 piece 6 to 1 auto

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -2,9 +2,11 @@ package frc.robot.autos;
 
 public enum AutoSelection {
   DO_NOTHING("", ""),
+
   AMP_5_PIECE("Red Amp 5 Piece", "Blue Amp 5 Piece"),
   SOURCE_4_PIECE("Red Source 4 Piece", "Blue Source 4 Piece"),
   SOURCE_OFFSET("Red Source Offset", "Blue Source Offset"),
+  MID_5_PIECE("Red 5 Piece 6 to 1","Blue 5 Piece 6 to 1"),
   OP("Red OP", "Blue OP");
 
   public final String redAutoName;
