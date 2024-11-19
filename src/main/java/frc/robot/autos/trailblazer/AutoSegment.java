23bf6a323ade8097b4d7fe3a84e3b2d9243f413a
Jonah Snider <jonah@jonahsnider.com>
2024-11-18 19:47:31 -0800

    Improve docs & parameter names in AutoSegment

--- src/main/java/frc/robot/autos/trailblazer/AutoSegment.java
+++ src/main/java/frc/robot/autos/trailblazer/AutoSegment.java
@@ -3,17 +3,25 @@ package frc.robot.autos.trailblazer;
 import frc.robot.autos.trailblazer.constraints.AutoPointConstraint;
 import java.util.List;
 
+/**
+ * A segment is a path (a continuous set of {@link AutoPoint points}) that the roobt will follow.
+ */
 public class AutoSegment {
   public final List<AutoPoint> points;
+
+  /**
+   * Constraints to apply to any points that don't have their own constraints specified. If a point
+   * specifies its own constraints, this field will be ignored.
+   */
   public final List<AutoPointConstraint> defaultConstraints;
 
-  public AutoSegment(List<AutoPointConstraint> globalConstraints, List<AutoPoint> points) {
-    this.defaultConstraints = globalConstraints;
+  public AutoSegment(List<AutoPointConstraint> defaultConstraints, List<AutoPoint> points) {
+    this.defaultConstraints = defaultConstraints;
     this.points = points;
   }
 
-  public AutoSegment(List<AutoPointConstraint> globalConstraints, AutoPoint... points) {
-    this(globalConstraints, List.of(points));
+  public AutoSegment(List<AutoPointConstraint> defaultConstraints, AutoPoint... points) {
+    this(defaultConstraints, List.of(points));
   }
 
   public AutoSegment(List<AutoPoint> points) {
