12a5986733dd5a28ca2860d136c3ede6631b989c
Owen <117956892+Owen756@users.noreply.github.com>
2024-11-19 17:20:46 -0800

    shooter rpm for demo + change team name in code

--- src/main/java/frc/robot/shooter/ShooterRpms.java
+++ src/main/java/frc/robot/shooter/ShooterRpms.java
@@ -1,9 +1,9 @@
 package frc.robot.shooter;
 
 public class ShooterRpms {
-  public static final double SUBWOOFER = 4000.0;
+  public static final double SUBWOOFER = 1250.0;
   public static final double DROP = 500.0;
-  public static final double PODIUM = 4000.0;
+  public static final double PODIUM = 2000.0;
   public static final double IDLE_WARMUP = 1000.0;
   public static final double PASS = 1000.0;
 }
