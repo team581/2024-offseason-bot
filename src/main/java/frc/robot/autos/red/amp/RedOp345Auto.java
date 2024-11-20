b21f3f02ac7d4411cbd9838012bce49ec35221fa
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-11-19 16:50:57 -0800

    format

--- src/main/java/frc/robot/autos/red/amp/RedOp345Auto.java
+++ src/main/java/frc/robot/autos/red/amp/RedOp345Auto.java
@@ -7,8 +7,8 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.autos.BaseAuto;
 import frc.robot.autos.trailblazer.AutoPoint;
 import frc.robot.autos.trailblazer.AutoSegment;
-import frc.robot.robot_manager.RobotCommands;
 import frc.robot.autos.trailblazer.Trailblazer;
+import frc.robot.robot_manager.RobotCommands;
 import frc.robot.robot_manager.RobotManager;
 
 public class RedOp345Auto extends BaseAuto {
@@ -22,13 +22,24 @@ public class RedOp345Auto extends BaseAuto {
         trailblazer.followSegment(
             new AutoSegment(
                 new AutoPoint(new Pose2d(15.811, 6.713, new Rotation2d(-170.80))),
-                new AutoPoint(new Pose2d(14.676, 6.767, new Rotation2d(-26.85)), Commands.sequence(autoCommands.speakerShotWithTimeout(), actions.intakeAssistCommand())),
+                new AutoPoint(
+                    new Pose2d(14.676, 6.767, new Rotation2d(-26.85)),
+                    Commands.sequence(
+                        autoCommands.speakerShotWithTimeout(), actions.intakeAssistCommand())),
                 new AutoPoint(new Pose2d(13.959, 6.872, new Rotation2d())),
-                new AutoPoint(new Pose2d(12.64, 7.063, new Rotation2d()), Commands.sequence(autoCommands.speakerShotWithTimeout(), actions.intakeAssistCommand())),
+                new AutoPoint(
+                    new Pose2d(12.64, 7.063, new Rotation2d()),
+                    Commands.sequence(
+                        autoCommands.speakerShotWithTimeout(), actions.intakeAssistCommand())),
                 new AutoPoint(new Pose2d(8.605, 7.449, new Rotation2d(0.0))),
-                new AutoPoint(new Pose2d(12.64, 7.063, new Rotation2d(-8.0)), autoCommands.speakerShotWithTimeout()),
-                new AutoPoint(new Pose2d(11.033, 6.879, new Rotation2d()), actions.intakeAssistCommand()),
+                new AutoPoint(
+                    new Pose2d(12.64, 7.063, new Rotation2d(-8.0)),
+                    autoCommands.speakerShotWithTimeout()),
+                new AutoPoint(
+                    new Pose2d(11.033, 6.879, new Rotation2d()), actions.intakeAssistCommand()),
                 new AutoPoint(new Pose2d(10.827, 6.42, new Rotation2d())),
-                new AutoPoint(new Pose2d(12.553, 6.036, new Rotation2d(-8.0)), autoCommands.speakerShotWithTimeout()))));
+                new AutoPoint(
+                    new Pose2d(12.553, 6.036, new Rotation2d(-8.0)),
+                    autoCommands.speakerShotWithTimeout()))));
   }
 }
