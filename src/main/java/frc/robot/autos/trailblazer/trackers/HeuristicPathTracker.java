1d6320f93c8752db5d4d3ad58bf007e37b044955
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-11-22 20:53:09 -0800

    heuristic path tracker

--- src/main/java/frc/robot/autos/trailblazer/trackers/HeuristicPathTracker.java
+++ src/main/java/frc/robot/autos/trailblazer/trackers/HeuristicPathTracker.java
@@ -8,6 +8,9 @@ import java.util.List;
 // TODO: Implement https://github.com/team581/2024-offseason-bot/issues/94
 public class HeuristicPathTracker implements PathTracker {
   private List<AutoPoint> points = List.of();
+  private Pose2d currentPose = new Pose2d();
+  private double proximityRadius = 0.5;
+  private int currentPointIndex = 0;
 
   @Override
   public void resetAndSetPoints(List<AutoPoint> points) {
@@ -15,16 +18,30 @@ public class HeuristicPathTracker implements PathTracker {
   }
 
   @Override
-  public void updateRobotState(Pose2d currentPose, ChassisSpeeds currentFieldRelativeRobotSpeeds) {}
+  public void updateRobotState(Pose2d currentPose, ChassisSpeeds currentFieldRelativeRobotSpeeds) {
+    this.currentPose = currentPose;
+  }
 
   @Override
   public Pose2d getTargetPose() {
-    return null;
+    if (isFinished() == true) {
+      return null;
+    }
+    AutoPoint currentPoint = points.get(getCurrentPointIndex());
+    Pose2d currentTargetPose = currentPoint.poseSupplier.get();
+    double distanceToTarget = currentPose.getTranslation().getDistance(currentTargetPose.getTranslation());
+
+    if (distanceToTarget < proximityRadius && currentPointIndex < points.size() -1) {
+      currentPointIndex++;
+    }
+
+
+    return points.get(currentPointIndex).poseSupplier.get();
   }
 
   @Override
   public int getCurrentPointIndex() {
-    return 0;
+    return currentPointIndex;
   }
 
   @Override
