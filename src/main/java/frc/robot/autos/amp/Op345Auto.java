8137d2c3ceeff57b6537039df7621c9505b20107
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-11-26 18:42:12 -0800

    add more auto segments to OP auto

--- src/main/java/frc/robot/autos/amp/Op345Auto.java
+++ src/main/java/frc/robot/autos/amp/Op345Auto.java
@@ -30,31 +30,21 @@ public class Op345Auto extends BaseAuto {
                     new Pose2d(15.78, 6.67, Rotation2d.fromDegrees(-60.48)))),
         trailblazer.followSegment(
             new AutoSegment(
-                new AutoPoint(new Pose2d(15.78, 6.67, Rotation2d.fromDegrees(-60.48))),
-                new AutoPoint(
-                    new Pose2d(14.676, 6.767, Rotation2d.fromDegrees(-26.85)),
-                    Commands.sequence(
-                        autoCommands.speakerShotWithTimeout(), actions.intakeAssistCommand())),
-                new AutoPoint(new Pose2d(13.68, 6.99, Rotation2d.fromDegrees(0.0))),
-                new AutoPoint(
-                    new Pose2d(12.64, 7.06, Rotation2d.fromDegrees(-21.67)),
-                    Commands.sequence(
-                        autoCommands.speakerShotWithTimeout(), actions.intakeAssistCommand())),
-                new AutoPoint(new Pose2d(8.52, 7.46, Rotation2d.fromDegrees(0.0))),
-                new AutoPoint(
-                    new Pose2d(12.64, 7.06, Rotation2d.fromDegrees(-21.67)),
-                    autoCommands.speakerShotWithTimeout()),
-                new AutoPoint(
-                    new Pose2d(11.03, 6.88, Rotation2d.fromDegrees(30.17)),
-                    actions.intakeAssistCommand()),
-                new AutoPoint(
-                    new Pose2d(10.14, 6.41, Rotation2d.fromDegrees(25.43)),
-                    actions.intakeAssistCommand()),
-                new AutoPoint(
-                    new Pose2d(8.32, 5.79, Rotation2d.fromDegrees(24.67)),
-                    actions.intakeAssistCommand()),
-                new AutoPoint(
-                    new Pose2d(12.64, 7.06, Rotation2d.fromDegrees(-21.67)),
-                    autoCommands.speakerShotWithTimeout()))));
+                new AutoPoint(new Pose2d(15.78, 6.67, Rotation2d.fromDegrees(-60.48))))),
+        trailblazer.followSegment(
+            new AutoSegment(
+                new AutoPoint(new Pose2d(14.676, 6.767, Rotation2d.fromDegrees(-26.85))))),
+        autoCommands.speakerShotWithTimeout(),
+        actions.intakeAssistCommand(),
+        trailblazer.followSegment(
+            new AutoSegment(new AutoPoint(new Pose2d(13.68, 6.99, Rotation2d.fromDegrees(0.0))))),
+        trailblazer.followSegment(
+            new AutoSegment(
+                new AutoPoint(new Pose2d(12.64, 7.06, Rotation2d.fromDegrees(-21.67))))),
+        autoCommands.speakerShotWithTimeout(),
+        actions.intakeAssistCommand(),
+        trailblazer.followSegment(
+            new AutoSegment(new AutoPoint(new Pose2d(8.52, 7.46, Rotation2d.fromDegrees(0.0))))),
+        autoCommands.speakerShotWithTimeout());
   }
 }
