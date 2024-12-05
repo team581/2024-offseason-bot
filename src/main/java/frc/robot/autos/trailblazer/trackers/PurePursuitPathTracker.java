c821c3369625c253b1d3d304fab2df382b4e5143
Simon <87281874+simonp17@users.noreply.github.com>
2024-12-05 13:11:25 -0800

    Start adding point processing

--- src/main/java/frc/robot/autos/trailblazer/trackers/PurePursuitPathTracker.java
+++ src/main/java/frc/robot/autos/trailblazer/trackers/PurePursuitPathTracker.java
@@ -12,7 +12,6 @@ public class PurePursuitPathTracker implements PathTracker {
   private static final double LOOKAHEAD_DISTANCE = 1.0;
   private List<AutoPoint> points = List.of();
   private Pose2d currentRobotPose = new Pose2d();
-  private double proximityRadius = 0.5;
   private Pose2d currentTargetWaypoint = new Pose2d();
   private Pose2d nextTargetWaypoint = new Pose2d();
   private int currentPointIndex = 0;
@@ -25,37 +24,67 @@ public class PurePursuitPathTracker implements PathTracker {
   @Override
   public void updateRobotState(Pose2d currentPose, ChassisSpeeds currentFieldRelativeRobotSpeeds) {
     this.currentRobotPose = currentPose;
-    AutoPoint currentPoint = points.get(getCurrentPointIndex());
-    currentTargetWaypoint = currentPoint.poseSupplier.get();
-    double distanceToTarget =
-        currentPose.getTranslation().getDistance(currentTargetWaypoint.getTranslation());
-    if (currentPointIndex < points.size() - 1) {
-      nextTargetWaypoint = points.get(currentPointIndex + 1).poseSupplier.get();
-      if (distanceToTarget < proximityRadius) {
-        currentPointIndex++;
-      }
-    }
     DogLog.log("Autos/Trailblazer/CurrentTargetPose", currentTargetWaypoint);
-    DogLog.log("Autos/Trailblazer/DistanceToTarget", distanceToTarget);
     DogLog.log("Autos/Trailblazer/NextPointIndex", currentPointIndex);
   }
 
   @Override
   public Pose2d getTargetPose() {
-    // TODO: Use for figuring out if a point is between the two waypoints for things like if the
-    // robot is behind or in front of path or if lookahead too far
-    // if (!((givenposex - startx) * (givenposex - endx) <= 0 && (givenposex - starty) * (givenposex
-    // - endy) <= 0)) {}
+    if (points.size() < 2) {
+      return points.get(0).poseSupplier.get();
+    }
 
-    var x1 = currentTargetWaypoint.getX();
-    var y1 = currentTargetWaypoint.getY();
-    var x2 = nextTargetWaypoint.getX();
-    var y2 = nextTargetWaypoint.getY();
-    double pathSlope = (y2 - y1) / (x2 - x1);
-
+    currentTargetWaypoint = points.get(getCurrentPointIndex()).poseSupplier.get();
+    if (getCurrentPointIndex() < points.size() - 1) {
+      nextTargetWaypoint = points.get(getCurrentPointIndex() + 1).poseSupplier.get();
+    } else {
+      return points.get(getCurrentPointIndex()).poseSupplier.get();
+    }
+    var startX = currentTargetWaypoint.getX();
+    var startY = currentTargetWaypoint.getY();
+    var endX = nextTargetWaypoint.getX();
+    var endY = nextTargetWaypoint.getY();
+    double pathSlope = (endY - startY) / (endX - startX);
     var perpendicularPoint =
         getPerpendicularPoint(currentTargetWaypoint, nextTargetWaypoint, currentRobotPose);
     var lookaheadPoint = getLookaheadPoint(perpendicularPoint, pathSlope, LOOKAHEAD_DISTANCE);
+
+    var lookaheadX = lookaheadPoint.getX();
+    var lookaheadY = lookaheadPoint.getY();
+    // Check if lookahead point is outside of the line segment
+    if (!((lookaheadX - startX) * (lookaheadX - endX) <= 0
+        && (lookaheadY - startY) * (lookaheadY - endY) <= 0)) {
+
+      var distanceToStart =
+          Math.sqrt(Math.pow(lookaheadX - startX, 2) + Math.pow(lookaheadY - startY, 2));
+      var distanceToEnd =
+          Math.sqrt(Math.pow(lookaheadX - endX, 2) + Math.pow(lookaheadY - endY, 2));
+      // Check if lookaheadpoint is outside on the starting side of the line
+      if (distanceToStart < distanceToEnd) {
+        // Target pose just becomes start point + lookahead
+        return getLookaheadPoint(currentTargetWaypoint, pathSlope, LOOKAHEAD_DISTANCE);
+      }
+
+      // Otherwise, the lookahead point is past the end point, which means we need to go around
+      // corner.
+      // TODO: Fix for case of perp point is also past end point
+      var perpDistanceToEnd =
+          Math.sqrt(
+              Math.pow(perpendicularPoint.getX() - endX, 2)
+                  + Math.pow(perpendicularPoint.getY() - endY, 2));
+      // check if we're at corner
+      if (getCurrentPointIndex() < points.size() - 2) {
+        var futurePoint = points.get(getCurrentPointIndex() + 2).poseSupplier.get();
+        double futureSlope = (futurePoint.getY() - endY) / (futurePoint.getX() - endX);
+        currentPointIndex++;
+        return getLookaheadPoint(
+            nextTargetWaypoint, futureSlope, LOOKAHEAD_DISTANCE - perpDistanceToEnd);
+      }
+      // otherwise just return the end point
+      else {
+        return new Pose2d(endX, endY, new Rotation2d());
+      }
+    }
     return lookaheadPoint;
   }
 
