09e3a5f83dc23b824373dfd46f876f44e5e7213a
Simon <87281874+simonp17@users.noreply.github.com>
2024-12-10 15:11:08 -0800

    Do more testing for perp and lookahead math

--- src/test/java/frc/robot/autos/trailblazer/trackers/pure_pursuit/PurePursuitUtilsTest.java
+++ src/test/java/frc/robot/autos/trailblazer/trackers/pure_pursuit/PurePursuitUtilsTest.java
@@ -9,23 +9,23 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public class PurePursuitUtilsTest {
   @Test
   void getPerpindicularWaypoint() {
-    var startPoint = new Pose2d(-4, 5, new Rotation2d());
-    var endPoint = new Pose2d(1, 0, new Rotation2d());
-    var robotPose = new Pose2d(0, 2, new Rotation2d());
+    var startPoint = new Pose2d(-1, -4, new Rotation2d());
+    var endPoint = new Pose2d(5, 2, new Rotation2d());
+    var robotPose = new Pose2d(-4.0, -4.0, new Rotation2d());
     var result = PurePursuitUtils.getPerpendicularPoint(startPoint, endPoint, robotPose);
 
-    var expected = new Pose2d(-0.5, 1.5, new Rotation2d());
+    var expected = new Pose2d(-2.5, -5.5, new Rotation2d());
     Assertions.assertEquals(expected, result);
   }
 
   @Test
   void getLookaheadWaypoint() {
-    var startPoint = new Pose2d(4, 0, new Rotation2d());
-    var endPoint = new Pose2d(4, 5, new Rotation2d());
-    var pointOnPath = new Pose2d(4, 2, new Rotation2d());
+    var startPoint = new Pose2d(0, 0, new Rotation2d());
+    var endPoint = new Pose2d(5, 0, new Rotation2d());
+    var pointOnPath = new Pose2d(2, 0, new Rotation2d());
     var result = PurePursuitUtils.getLookaheadPoint(startPoint, endPoint, pointOnPath, 1.0);
 
-    var expected = new Pose2d(4, 3, new Rotation2d());
+    var expected = new Pose2d(3, 0, new Rotation2d());
     Assertions.assertEquals(expected, result);
   }
 }
