953ba0e1602446e65c2145394d9abf58062aeb92
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-12-10 17:20:07 -0800

    increase rotation p for trailblazer

--- src/main/java/frc/robot/autos/trailblazer/Trailblazer.java
+++ src/main/java/frc/robot/autos/trailblazer/Trailblazer.java
@@ -31,7 +31,7 @@ public class Trailblazer {
   private final PathTracker pathTracker = new HeuristicPathTracker();
   private final PathFollower pathFollower =
       new PidPathFollower(
-          new PIDController(4, 0, 0), new PIDController(4, 0, 0), new PIDController(2.5, 0, 0));
+          new PIDController(4, 0, 0), new PIDController(4, 0, 0), new PIDController(8.0, 0, 0));
   private int previousAutoPointIndex = -1;
   private ChassisSpeeds previousSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
   private double previousTimestamp = 0.0;
