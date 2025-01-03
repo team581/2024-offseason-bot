291d178324b294ad16732275a07e8b013e2cfa04
Simon <87281874+simonp17@users.noreply.github.com>
2025-01-02 19:38:48 -0800

    Make changes to the test auto

--- src/main/java/frc/robot/autos/amp/TestAuto.java
+++ src/main/java/frc/robot/autos/amp/TestAuto.java
@@ -8,6 +8,7 @@ import frc.robot.autos.BaseAuto;
 import frc.robot.autos.trailblazer.AutoPoint;
 import frc.robot.autos.trailblazer.AutoSegment;
 import frc.robot.autos.trailblazer.Trailblazer;
+import frc.robot.autos.trailblazer.constraints.AutoConstraintOptions;
 import frc.robot.robot_manager.RobotManager;
 
 public class TestAuto extends BaseAuto {
@@ -27,25 +28,17 @@ public class TestAuto extends BaseAuto {
         Commands.runOnce(
             () ->
                 robotManager.localization.resetPose(
-                    new Pose2d(15.18, 6.41, Rotation2d.fromDegrees(0.0)))),
+                    new Pose2d(15, 7, Rotation2d.fromDegrees(0.0)))),
         trailblazer.followSegment(
             new AutoSegment(
-                new AutoPoint(new Pose2d(12.00, 6.28, Rotation2d.fromDegrees(-100))),
-                new AutoPoint(new Pose2d(10.00, 5.28, Rotation2d.fromDegrees(40))),
-                new AutoPoint(new Pose2d(8.00, 6.28, Rotation2d.fromDegrees(-50))),
-                new AutoPoint(new Pose2d(6.00, 5.28, Rotation2d.fromDegrees(100))),
-                new AutoPoint(new Pose2d(4.00, 6.28, Rotation2d.fromDegrees(0)))
-
-                )),
-        trailblazer.followSegment(
-            new AutoSegment(new AutoPoint(new Pose2d(11.82, 6.59, Rotation2d.fromDegrees(24.0))))),
-        trailblazer.followSegment(
-            new AutoSegment(
-                new AutoPoint(new Pose2d(9.98, 6.55, Rotation2d.fromDegrees(-5.0))),
-                new AutoPoint(new Pose2d(8.60, 5.85, Rotation2d.fromDegrees(100.0))))),
-        trailblazer.followSegment(
-            new AutoSegment(
-                new AutoPoint(new Pose2d(10.78, 7.34, Rotation2d.fromDegrees(-80.0))),
-                new AutoPoint(new Pose2d(13.3, 6.59, Rotation2d.fromDegrees(40.0))))));
+                new AutoPoint(
+                    new Pose2d(10, 7, Rotation2d.fromDegrees(0)),
+                    new AutoConstraintOptions(false, 100, 100, 7, 500)),
+                new AutoPoint(
+                    new Pose2d(10, 5, Rotation2d.fromDegrees(0)),
+                    new AutoConstraintOptions(false, 500, 500, 7, 500)),
+                new AutoPoint(
+                    new Pose2d(14, 5, Rotation2d.fromDegrees(0)),
+                    new AutoConstraintOptions(false, 500, 500, 7, 500)))));
   }
 }
